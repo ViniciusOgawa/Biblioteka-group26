5e0ee51fd34fdfe1e1cf91c2ebb9447f3066f5d3
ViniciusOgawa <vini_ogawa@hotmail.com>
2023-05-04 11:19:00 -0300

    feat: created app following

--- users/views.py
+++ users/views.py
@@ -4,22 +4,4 @@ from users.serializers import UserSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
-
-
-class UserView(APIView):
-    def post(self, request: Request) -> Response:
-        serializer = UserSerializer(data=request.data)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-
-        serializer.save()
-
-        return Response(serializer.data, status.HTTP_201_CREATED)
-
-    def get(self, request: Request) -> Response:
-        users = User.objects.all()
-
-        serializer = UserSerializer(users, many=True)
-
-        return Response(serializer.data, status.HTTP_200_OK)
+from rest_framework import generics
