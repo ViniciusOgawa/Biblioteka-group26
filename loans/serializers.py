b40d930d3527ebdbdf763bde3d5dc98530947eda
GabrielRf <ti.gabrielrf@gmail.com>
2023-05-04 18:09:11 -0300

    Feat: add new app loans with model and serializer loan

---
+++ loans/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Loan
+
+
+class LoanSerialzier(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(read_only=True, source="user.id")
+    copy_id = serializers.IntegerField(read_only=True, source="copy.id")
+
+    class Meta:
+        model = Loan
+        fields = ["id", "return_date", "user_id", "copy_id"]
+
+    def create(self, validated_data):
+        return Loan.objects.create(**validated_data)
