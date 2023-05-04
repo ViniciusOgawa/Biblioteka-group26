24272b10ee6b1b09d2d7fa7f80dfbba822db650b
ViniciusOgawa <vini_ogawa@hotmail.com>
2023-05-04 15:18:24 -0300

    fix: adjusting models

--- books/models.py
+++ books/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
+
+    users = models.ManyToManyField("users.Users", related_name="books_following")
