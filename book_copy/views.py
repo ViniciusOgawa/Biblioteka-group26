3f3742d44c0cdb7216c27dfc263c1d2f0ce56924
GabrielRf <ti.gabrielrf@gmail.com>
2023-05-04 17:46:42 -0300

    Feat: add Copy model with CopySerializer and ListAPIView for book_copy views

--- book_copy/views.py
+++ book_copy/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import CopySerializer
+from .models import Copy
 
-# Create your views here.
+
+class CopyView(ListAPIView):
+    serializer_class = CopySerializer
+    pagination_class = PageNumberPagination
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        book_id = self.kwargs.get("pk")
+        return Copy.objects.filter(book_id=book_id)
