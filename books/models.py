dc4fbd1b665fcf3e1259156b2ea0847ac085e7a1
Jefferson de Barros Oliveira <102183069+JeffOliveiraDev@users.noreply.github.com>
2023-05-06 11:12:55 -0300

    feat: fixed following routes

--- books/models.py
+++ books/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=50, default=None)
     author = models.CharField(max_length=50, default=None)
+
+    users = models.ManyToManyField("users.User", related_name="books_following")
